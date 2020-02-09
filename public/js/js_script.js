2bffdb39967bca523dd2fda74423678b9ab56773
jake991 <jacoub96_2011@hotmail.com>
2020-02-10 00:07:37 +0100

    pure javascript 4-8

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -5,20 +5,32 @@ function  menuItem(name, ingreds, kcal, allergy, image){
     this.allergy=allergy;
     this.image=image;
 
-    this.getNameAndKcal = function(){
-        return this.name + " " + this.kcal + " Calories";
+    this.getName = function(){
+        return this.name;
+    }
+
+    this.getIngreds = function(){
+        return this.ingreds;
+    }
+
+    this.getKcal = function(){
+        return this.kcal + " Calories";
+    }
+
+    this.getImage = function(){
+        return this.image;
     }
     
     this.getAllergy = function(){
     		return this.allergy;
     }
-  
+    
 }
 
 
 let burger1 = new menuItem("Cheese and Bacon Burger", "Contains: cheese, bacon, meet and fries.", 923, "contains gluten.", "https://i1.wp.com/www.foodrepublic.com/wp-content/uploads/2012/03/baconbluecheeseburger.jpg?resize=700%2C%20466&ssl=1");
 
-let burger2 = new menuItem("Umami Burger", "Contains: cheese, bacon, meet and fries.", 923, "contains gluten.", "https://mealhack.com/wp-content/uploads/2014/09/umami-dusted-burger_0008-680x453.jpg");
+let burger2 = new menuItem("Umami Burger", "Contains: cheese, bacon, meet, umami and fries.", 923, "contains gluten.", "https://mealhack.com/wp-content/uploads/2014/09/umami-dusted-burger_0008-680x453.jpg");
 
 let burger3 = new menuItem("Vegge Burger","Contains: flafel and fries.",700, null, "https://assets.bonappetit.com/photos/57acae2d1b33404414975121/master/pass/ultimate-veggie-burger.jpg");
 
@@ -29,10 +41,34 @@ let burger5 = new menuItem("Chorizo Burger", "Contains: Chorizo sausages.", 940,
 var burgers = [burger1, burger2, burger3, burger4, burger5];
 
 for(let burger of  burgers){
-let par = document.createElement('p');
-par.appendChild(document.createTextNode(burger.getNameAndKcal()));
-if(burger.getAllergy() != null){
-par.appendChild(document.createTextNode(" " + burger.getAllergy()));
+    let div = document.createElement('div');
+    let heading = document.createElement('h3');
+    heading.appendChild(document.createTextNode(burger.getName()));
+    let image = document.createElement('img');
+    image.src = burger.getImage();
+    image.width = '200';
+    image.length= '200';
+    let list = document.createElement('ul');
+    let item1 = document.createElement('li');
+    let par1 = document.createElement('p');
+    par1.appendChild(document.createTextNode(burger.getIngreds()));
+    item1.appendChild(par1);
+    list.appendChild(item1);
+    let item2 = document.createElement('li');
+    let par2 = document.createElement('p');
+    par2.appendChild(document.createTextNode(burger.getKcal()));
+    item2.appendChild(par2);
+    list.appendChild(item2);
+    if(burger.getAllergy() != null){
+        let item3 = document.createElement('li');
+        let par3 = document.createElement('p');
+        par3.appendChild(document.createTextNode(burger.getAllergy()));
+        item3.appendChild(par3);
+        list.appendChild(item3);
+    }
+    div.appendChild(heading);
+    div.appendChild(image);
+    div.appendChild(list);
+    document.getElementById('wrapperID').appendChild(div);
 }
-document.getElementById('myID').appendChild(par);
-}
+
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,7 +1,7 @@
- const vm = new Vue({
+/* const vm = new Vue({
    el: '#myID',
    data: {
    burgers: [{name: 'Cheese and Bacon Burger', calories: '923', allergy: 'contains gluten', all_check: true}, {name: 'Umami Burger', calories: '923', allergy: 'contains gluten', all_check: true}, {name: 'Vegge Burger', calories: '700', allergy: null, all_check: false}, {name: 'American Stake', calories: '950', allergy: null, all_check: false}, {name: 'Chorizo Burger', calories: '940', allergy: null, all_check: false}]
    }
  })
-
+*/

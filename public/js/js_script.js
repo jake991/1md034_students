df141ff23d827f92b4083c8c5c6e60d091c92b3a
jake991 <jacoub96_2011@hotmail.com>
2020-02-09 20:06:19 +0100

    Task 4-7 completed

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -6,44 +6,33 @@ function  menuItem(name, ingreds, kcal, allergy, image){
     this.image=image;
 
     this.getNameAndKcal = function(){
-        return this.name + " " + this.kcal
+        return this.name + " " + this.kcal + " Calories";
+    }
+    
+    this.getAllergy = function(){
+    		return this.allergy;
     }
   
 }
 
 
-let burger1 = new menuItem("Cheese and Bacon Burger", "Contains: cheese, bacon, meet and fries.", 923, "Allergy: contain gluten.", "https://i1.wp.com/www.foodrepublic.com/wp-content/uploads/2012/03/baconbluecheeseburger.jpg?resize=700%2C%20466&ssl=1");
+let burger1 = new menuItem("Cheese and Bacon Burger", "Contains: cheese, bacon, meet and fries.", 923, "contains gluten.", "https://i1.wp.com/www.foodrepublic.com/wp-content/uploads/2012/03/baconbluecheeseburger.jpg?resize=700%2C%20466&ssl=1");
 
-let burger2 = new menuItem("Umami Burger", "Contains: cheese, bacon, meet and fries.", 923, "Allergy: contain gluten.", "https://mealhack.com/wp-content/uploads/2014/09/umami-dusted-burger_0008-680x453.jpg");
+let burger2 = new menuItem("Umami Burger", "Contains: cheese, bacon, meet and fries.", 923, "contains gluten.", "https://mealhack.com/wp-content/uploads/2014/09/umami-dusted-burger_0008-680x453.jpg");
 
-let burger3 = new menuItem("Vegge Burger","Contains: flafel and fries.",700, "Allergy: gluten free and lactose free.", "https://assets.bonappetit.com/photos/57acae2d1b33404414975121/master/pass/ultimate-veggie-burger.jpg");
+let burger3 = new menuItem("Vegge Burger","Contains: flafel and fries.",700, null, "https://assets.bonappetit.com/photos/57acae2d1b33404414975121/master/pass/ultimate-veggie-burger.jpg");
 
-let burger4 = new menuItem("American Stack", "Contains: ribs with BBQ sauce.", 950, "Allergy: gluten free and lactose free", "https://i.pinimg.com/originals/3b/af/2c/3baf2c99ed5978491503248ac505c989.jpg");
+let burger4 = new menuItem("American Stack", "Contains: ribs with BBQ sauce.", 950, null, "https://i.pinimg.com/originals/3b/af/2c/3baf2c99ed5978491503248ac505c989.jpg");
 
-let burger5 = new menuItem("Chorize Burger", "Contains: Chorizo sausages.", 940, "Allergy: gluten free and lactose free", "https://www.kitchenkonfidence.com/wp-content/uploads/2015/08/Chorizo-Cheddar-Burger.jpg");
+let burger5 = new menuItem("Chorizo Burger", "Contains: Chorizo sausages.", 940, null, "https://www.kitchenkonfidence.com/wp-content/uploads/2015/08/Chorizo-Cheddar-Burger.jpg");
 
-let txt1 = document.createTextNode(burger1.getNameAndKcal());
-let txt2 = document.createTextNode(burger2.getNameAndKcal());
-let txt3 = document.createTextNode(burger3.getNameAndKcal());
-let txt4 = document.createTextNode(burger4.getNameAndKcal());
-let txt5 = document.createTextNode(burger5.getNameAndKcal());
+var burgers = [burger1, burger2, burger3, burger4, burger5];
 
-let par1 = document.createElement('p');
-let par2 = document.createElement('p');
-let par3 = document.createElement('p');
-let par4 = document.createElement('p');
-let par5 = document.createElement('p');
-
-par1.appendChild(txt1);
-par2.appendChild(txt2);
-par3.appendChild(txt3);
-par4.appendChild(txt4);
-par5.appendChild(txt5);
-
-document.getElementById('myID').appendChild(par1);
-document.getElementById('myID').appendChild(par2);
-document.getElementById('myID').appendChild(par3);
-document.getElementById('myID').appendChild(par4);
-document.getElementById('myID').appendChild(par5);
-
-
+for(let burger of  burgers){
+let par = document.createElement('p');
+par.appendChild(document.createTextNode(burger.getNameAndKcal()));
+if(burger.getAllergy() != null){
+par.appendChild(document.createTextNode(" " + burger.getAllergy()));
+}
+document.getElementById('myID').appendChild(par);
+}
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,11 +1,7 @@
-const vm = new Vue({
+ const vm = new Vue({
    el: '#myID',
    data: {
-   burger1: 'Cheese and Bacon Burger 923',
-   burger2: 'Umami Burger 923',
-   burger3: 'Vegge Burger 700',
-   burger4: 'American Stake 950',
-   burger5: 'Chorizo BUrger 940'
+   burgers: [{name: 'Cheese and Bacon Burger', calories: '923', allergy: 'contains gluten', all_check: true}, {name: 'Umami Burger', calories: '923', allergy: 'contains gluten', all_check: true}, {name: 'Vegge Burger', calories: '700', allergy: null, all_check: false}, {name: 'American Stake', calories: '950', allergy: null, all_check: false}, {name: 'Chorizo Burger', calories: '940', allergy: null, all_check: false}]
    }
-
  })
+

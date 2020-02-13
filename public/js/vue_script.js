41dc0e9add16240b12b613e19c63aed76c9c02b3
jake991 <jacoub96_2011@hotmail.com>
2020-02-13 19:13:48 +0100

    Task 5-3 completed

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -4,6 +4,17 @@ const vm = new Vue({
     el: '#main',
     data: {
 
+        local_object: {
+            details:{
+                x: 0,
+                y: 0,
+            }
+        },
+
+        orderId: 0,
+
+        orderItems: [],
+        
         orders: {},
         
         width: 200,
@@ -28,33 +39,17 @@ const vm = new Vue({
         fname: "",
         email: "",
     },
-    created: function() {
-        /* When the page is loaded, get the current orders stored on the server.
-         * (the server's code is in app.js) */
-        socket.on('initialize', function(data) {
-            this.orders = data.orders;
-        }.bind(this));
-
-        /* Whenever an addOrder is emitted by a client (every open map.html is
-         * a client), the server responds with a currentQueue message (this is
-         * defined in app.js). The message's data payload is the entire updated
-         * order object. Here we define what the client should do with it.
-         * Spoiler: We replace the current local order object with the new one. */
-        socket.on('currentQueue', function(data) {
-            this.orders = data.orders;
-        }.bind(this));
-    },
     
     methods: {
         getClicked: function (){
             this.clicked = true;
-            radio = document.getElementsByName("gender");
-            lene = radio.length;
+            var radio = document.getElementsByName("gender");
+            var leng = radio.length;
             this.gender_1 = null;
             this.gender_2 = null;
             this.gender_3 = null;
             this.gender_4 = null;
-            for(i = 0; i<lene; ++i){
+            for(var i = 0; i<leng; ++i){
                 if(radio[i].checked){
                     switch (i){
                     case 0:
@@ -74,30 +69,36 @@ const vm = new Vue({
                 }
             }
             
-            checkboxes = document.getElementsByName("check");
-            len = checkboxes.length;
+            var checkboxes = document.getElementsByName("check");
+            var len = checkboxes.length;
             this.burger1 = null;
             this.burger2 = null;
             this.burger3 = null;
             this.burger4 = null;
             this.burger5 = null;
-            for(i = 0; i<len; ++i){
+            this.orderItems = [];
+            for(var i = 0; i<len; ++i){
                 if(checkboxes[i].checked){
                     switch (i){
                     case 0:
                         this.burger1 = this.food[0].name;
+                        this.orderItems.push(this.burger1);
                         break;
                     case 1:
                         this.burger2 = this.food[1].name;
+                        this.orderItems.push(this.burger2);
                         break;
                     case 2:
                         this.burger3 = this.food[2].name;
+                        this.orderItems.push(this.burger3);
                         break;
                     case 3:
                         this.burger4 = this.food[3].name;
+                        this.orderItems.push(this.burger4);
                         break;
                     case 4:
                         this.burger5 = this.food[4].name;
+                        this.orderItems.push(this.burger5);
                         break;
 
                     }
@@ -110,32 +111,39 @@ const vm = new Vue({
             /* This function returns the next available key (order number) in
              * the orders object, it works under the assumptions that all keys
              * are integers. */
-            let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
-                return Math.max(last, next);
-            }, 0);
-            return lastOrder + 1;
+            // let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+            //     return Math.max(last, next);
+            // }, 0);
+            // return lastOrder + 1;
+            this.orderId += 1;
+            return this.orderId;
         },
-        addOrder: function(event) {
+        addOrder: function() {
             /* When you click in the map, a click event object is sent as parameter
              * to the function designated in v-on:click (i.e. this one).
              * The click event object contains among other things different
              * coordinates that we need when calculating where in the map the click
              * actually happened. */
+            socket.emit('addOrder', {
+                orderId: this.getNext(),
+                details: {
+                    x: this.local_object.details.x,
+                    y: this.local_object.details.y,
+                },
+                orderItems: this.orderItems
+            });
+        },
+
+        displayOrder: function(event) {
             let offset = {
                 x: event.currentTarget.getBoundingClientRect().left,
                 y: event.currentTarget.getBoundingClientRect().top,
             };
-            socket.emit('addOrder', {
-                orderId: this.getNext(),
-                details: {
-                    x: event.clientX - 10 - offset.x,
-                    y: event.clientY - 10 - offset.y,
-                },
-                orderItems: ['Beans', 'Curry'],
-            });
+            
+            this.local_object.details.x =   event.clientX - 10 - offset.x;
+            this.local_object.details.y =   event.clientY - 10 - offset.y;
         },
 
-    }
-    
+    }    
 
 })
